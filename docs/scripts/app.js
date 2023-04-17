285ebe288f3f0a4951450208ad208764c8673760
BigMak <mail@example.com>
2023-04-17 20:50:01 +0600

    add menu render functions

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -3,6 +3,11 @@
 const habbits = [];
 const habbitKey = 'habbits';
 
+/* page */
+const page = {
+  menu: document.querySelector('.menu__list'),
+};
+
 /* utils */
 function loadData() {
   habbits.length = 0;
@@ -19,6 +24,47 @@ function saveData() {
   localStorage.setItem(habbitKey, JSON.stringify(habbits));
 }
 
+/* render */
+function renderMenu(activeHabbit) {
+  if (!activeHabbit) {
+    return;
+  }
+  for (const habbit of habbits) {
+    const exists = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
+    if (!exists) {
+      const element = document.createElement('button');
+      element.setAttribute('menu-habbit-id', habbit.id);
+      element.classList.add('menu__item');
+      element.addEventListener('click', () => {
+        render(habbit.id);
+      });
+      element.innerHTML = `<img src="./images/${habbit.icon}.svg" alt="{Habbit.name}" />`;
+      if (activeHabbit.id === habbit.id) {
+        element.classList.add('menu__item_active');
+      }
+      page.menu.appendChild(element);
+      continue;
+    }
+    if (activeHabbit.id === habbit.id) {
+      exists.classList.add('menu__item_active');
+    } else {
+      exists.classList.remove('menu__item_active');
+    }
+  }
+}
+
+function render(activeHabbitId) {
+  const activeHabbit = habbits.find((habbit) => habbit.id === activeHabbitId);
+  if (!activeHabbit) {
+    return;
+  }
+  renderMenu(activeHabbit);
+}
+
+/* init */
 (() => {
   loadData();
+  if (habbits) {
+    render(habbits[0].id);
+  }
 })();
